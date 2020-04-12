a974c61e11a35813a9eeb0dbe96be268cd364f9e
Juanperdomolol <juanpb902@gmail.com>
2020-04-11 19:40:06 -0500

    este es el 8 ejercicio

---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+#Unir listas
+#Generar primero una lista con los números entre 0 y 10, 
+# luego generar otra lista con los números del 11 al 20. 
+# Unir ambas lista e imprimir el resultado.
+listaA = list(range(11))
+listaB = list(range(10,21))
+print(listaA+listaB)
