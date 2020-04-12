f65271423c8443598a55fc136316c9bebd5c7692
Juanperdomolol <juanpb902@gmail.com>
2020-04-11 20:03:20 -0500

    este es el 10 ejercicio

---
+++ ejercicio10.py
@@ -0,0 +1,10 @@
+#Media de dos números
+#Calcular la media de dos números.
+import random
+numero1 = random.randrange(1,50)
+numero2 = random.randrange(1,50)
+
+print("numero1 =",numero1 ,"numero2 = ",numero2)
+media = (numero1+numero2)/2
+
+print(media)
